53c0eb4fd8b8fcaab21f80afa31feb0ac2d26fe1
wxmimperio <wxmimperio@outlook.com>
2016-11-08 00:03:41 +0800

    签名验证

--- src/main/java/com/wxmimperio/wechat/controller/CheckSignature.java
+++ src/main/java/com/wxmimperio/wechat/controller/CheckSignature.java
@@ -1,13 +1,16 @@
 package com.wxmimperio.wechat.controller;
 
 import com.wxmimperio.wechat.service.ISignatureService;
+import com.wxmimperio.wechat.service.impl.SignatureServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by wxmimperio on 2016/11/6.
@@ -16,25 +19,29 @@ import java.io.IOException;
 @Controller
 public class CheckSignature {
 
-    private ISignatureService signatureService;
+    private SignatureServiceImpl signatureServiceImpl;
 
     @RequestMapping(value = "checkSignature")
     @ResponseBody
     public void createTableController(@RequestParam(value = "signature", required = false) String signature,
                                       @RequestParam(value = "timestamp", required = false) String timestamp,
                                       @RequestParam(value = "nonce", required = false) String nonce,
-                                      @RequestParam(value = "echostr", required = false) String echostr) throws IOException {
+                                      @RequestParam(value = "echostr", required = false) String echostr, HttpServletResponse response) throws IOException {
 
         try {
-            this.signatureService.checkSignature(signature,timestamp,nonce);
+            PrintWriter out = response.getWriter();
+            if (this.signatureServiceImpl.checkSignature(signature, timestamp, nonce)) {
+                out.print(echostr);
+            }
+            out.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println("this is a checkSignature");
     }
 
-    @Resource
-    public void setSignatureService(ISignatureService signatureService) {
-        this.signatureService = signatureService;
+    @Resource(name = "signatureServiceImpl")
+    public void setSignatureService(SignatureServiceImpl signatureServiceImpl) {
+        this.signatureServiceImpl = signatureServiceImpl;
     }
 }
