307a0dc53f38ba1dc9332a4335b96c0074fb41fc
wxmimperio <wxmimperio@outlook.com>
2016-11-07 23:34:55 +0800

    签名验证

--- src/main/java/com/wxmimperio/wechat/controller/CheckSignature.java
+++ src/main/java/com/wxmimperio/wechat/controller/CheckSignature.java
@@ -1,10 +1,12 @@
 package com.wxmimperio.wechat.controller;
 
+import com.wxmimperio.wechat.service.ISignatureService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 
 /**
@@ -14,16 +16,25 @@ import java.io.IOException;
 @Controller
 public class CheckSignature {
 
+    private ISignatureService signatureService;
+
     @RequestMapping(value = "checkSignature")
     @ResponseBody
     public void createTableController(@RequestParam(value = "signature", required = false) String signature,
                                       @RequestParam(value = "timestamp", required = false) String timestamp,
                                       @RequestParam(value = "nonce", required = false) String nonce,
                                       @RequestParam(value = "echostr", required = false) String echostr) throws IOException {
-        System.out.println(signature);
-        System.out.println(timestamp);
-        System.out.println(nonce);
-        System.out.println(echostr);
+
+        try {
+            this.signatureService.checkSignature(signature,timestamp,nonce);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("this is a checkSignature");
+    }
+
+    @Resource
+    public void setSignatureService(ISignatureService signatureService) {
+        this.signatureService = signatureService;
     }
 }
--- src/main/java/com/wxmimperio/wechat/service/impl/SignatureServiceImpl.java
+++ src/main/java/com/wxmimperio/wechat/service/impl/SignatureServiceImpl.java
@@ -2,6 +2,7 @@ package com.wxmimperio.wechat.service.impl;
 
 import com.wxmimperio.wechat.service.ISignatureService;
 
+import java.security.MessageDigest;
 import java.util.Arrays;
 
 /**
@@ -15,8 +16,32 @@ public class SignatureServiceImpl implements ISignatureService {
         //对token、timestamp、nonce按照字典排序
         String[] paramArr = new String[]{token, timestamp, nonce};
         Arrays.sort(paramArr);
-        return false;
+        StringBuilder contextBuilder = new StringBuilder();
+        for (String param : paramArr) {
+            contextBuilder.append(param);
+        }
+        String checkText = null;
+
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        byte[] digest = md.digest(contextBuilder.toString().getBytes());
+        checkText = byteToStr(digest);
+        return checkText != null && checkText.equals(signature.toUpperCase());
     }
 
+    private String byteToStr(byte[] byteArray) {
+        String str = "";
+        for (int i = 0; i < byteArray.length; i++) {
+            str += byteToHexStr(byteArray[i]);
+        }
+        return str;
+    }
 
+    private String byteToHexStr(byte mByte) {
+        char[] digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F'};
+        char[] tempArr = new char[2];
+        tempArr[0] = digit[(mByte >>> 4) & 0X0F];
+        tempArr[1] = digit[mByte & 0X0F];
+        return new String(tempArr);
+    }
 }
